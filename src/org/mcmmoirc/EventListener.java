4d232ff6e1e63851f9b5605bef7fb9a4150da563
jgeboski <jgeboski@gmail.com>
2012-02-02 13:33:18 -0500

    EventListener: fixed typo

--- src/org/mcmmoirc/EventListener.java
+++ src/org/mcmmoirc/EventListener.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.mcmmoirc;;
+package org.mcmmoirc;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
