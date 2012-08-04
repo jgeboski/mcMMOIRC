ea2803a30471da9f46f13698ba46c42ea632ad83
jgeboski <jgeboski@gmail.com>
2012-08-04 16:21:05 -0400

    EventListener: Use AsyncPlayerChatEvent as PlayerChatEvent is now deprecated

--- src/main/java/org/mcmmoirc/EventListener.java
+++ src/main/java/org/mcmmoirc/EventListener.java
@@ -21,7 +21,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.PluginManager;
 
@@ -47,7 +47,7 @@ public class EventListener implements Listener
     }
     
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
-    public void onPlayerChat(PlayerChatEvent e)
+    public void onPlayerChat(AsyncPlayerChatEvent e)
     {
         PlayerProfile pp;
         String msg, party;
